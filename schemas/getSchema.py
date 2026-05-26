bafe98fb21b2ca27595b39b8b2250bfbf7a3fde4
Queiroz JR <131998280+josielqrozjr@users.noreply.github.com>
2026-05-26 14:37:28 -0300

    fix: add categorical features definition to getSchema.py

--- schemas/getSchema.py
+++ schemas/getSchema.py
@@ -9,3 +9,11 @@ FROM '../data/raw/parquet/train/*.parquet'; -- Ajuste o caminho para um dos arqu
 """).fetchdf()
 
 schema.to_csv("schema.csv", index=False)
+
+"""
+CATEGORICAL_FEATURES = [
+    'B_30', 'B_38', 'D_114', 'D_116',
+    'D_117', 'D_120', 'D_126',
+    'D_63', 'D_64', 'D_66', 'D_68'
+]
+"""
